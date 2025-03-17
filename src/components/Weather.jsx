0914f14d2942b703cf82944c1c71bbb10b34df24
charuka96 <chaukathisaru96@gmail.com>
2025-03-17 16:47:30 +0530

    created icon cod

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -7,26 +7,54 @@ import drizzle_icon from '../assets/drizzle.png'
 import rain_icon from '../assets/rain.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
+import snow_icon from '../assets/snow.png'
 
 const Weather = () => {
+
+const [weatherData ,setWeatherData] = useState(false);
+ const allIcons = {
+    "01d":clear_icon,
+    "01n":clear_icon,
+    "02d":cloud_icon,
+    "02n":cloud_icon,
+    "03d":cloud_icon,
+    "03n":cloud_icon,
+    "04n":drizzle_icon,
+    "09d":rain_icon,
+    "09n":rain_icon,
+    "10d":rain_icon,
+    "10n":rain_icon,
+    "13d":rain_icon,
+    "13n":snow_icon,
+ }
+
 const search  =  async (city)=>{
     try {
-        
-    
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
 
         const response = await fetch(url);
         const data = await response.json();
-
         console.log(data);
 
+        const icon = allIcons[data.Weather[0].icon] || clear_icon
+
+        setWeatherData({
+            humidity :data.main.humidity,
+            windSpeed:data.main.speed,
+            temprature:  Math.floor (data.main.temp),
+            location:data.name,
+            icon: icon
+        })
+
     } catch (error) {
+
+        console.log(error);
   
     }
 }
 
 useEffect(()=>{
-    search("London")
+    search("New Yourk")
 },[])
 
   return (
@@ -37,13 +65,13 @@ useEffect(()=>{
         </div>
      
      <img src={clear_icon} alt=""  className='wather-icon' />
-     <p className='tempraturre' >16°C</p>
-     <p className='location'>London</p>
+     <p className='tempraturre' >{weatherData.temprature}</p>
+     <p className='location'>{weatherData.location}</p>
      <div className="weather-data">
         <div className="col">
             <img src={humidity_icon} alt="" />
             <div>
-                <p>91%</p>
+                <p>{weatherData.humidity}%</p>
                 <span>Humidity</span>
             </div>
         </div>
@@ -51,7 +79,7 @@ useEffect(()=>{
         <div className="col">
             <img src={wind_icon} alt="" />
             <div>
-                <p>3.6Km/h</p>
+                <p>{weatherData.windSpeed}Km/h</p>
                 <span>Wind Speed</span>
             </div>
         </div>
